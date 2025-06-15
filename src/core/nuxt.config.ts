dfb5a846f2a12b86f9d752075ce2538ec2690b59
jph6366 <jphardee@gmail.com>
2025-06-15 13:26:48 -0400

    add interceptors in

--- src/core/nuxt.config.ts
+++ src/core/nuxt.config.ts
@@ -5,19 +5,6 @@ const currentDir = dirname(fileURLToPath(import.meta.url))
 console.log('Adding layer: core')
 // site feels like a nice bucket here for non-specific concerns
 export default defineNuxtConfig({
-  components: [
-    {
-      // can be relative, alias or absolute
-      path: 'components',
-
-      // components organised in sub-folders should not be prefixed
-      pathPrefix: false,
-
-      // ensure contact form is accessible in Nuxt content
-      // the alternative would be to rename it ContactForm.global.vue
-    //   global: true
-    },
-  ],
   css: [ join(currentDir, './assets/app.css') ],
   dir: {
     assets: 'assets',
